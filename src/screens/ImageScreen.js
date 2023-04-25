cdf5a29b91a49a652390f11d3d65d3431f66015a
Sajal Kaushal <sajalkaushal3@gmail.com>
2023-04-25 15:18:21 +0530

    learning parent child relationship

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,10 +1,21 @@
 import React from "react";
 import {View,Text,StyleSheet,Image} from 'react-native'
-
+import ImageDetail from "./components/ImageDetails";
 const ImageScreen = () => {
     return(
         <View>
-            <Text>Image Screen</Text>
+            <ImageDetail 
+            title="Forest" 
+            imagesource={require("../../assets/forest.jpg")}
+            />
+            <ImageDetail 
+            title="Beach"  
+            imagesource={require("../../assets/beach.jpg")}
+            />
+            <ImageDetail 
+            title="Mountain" 
+            imagesource={require("../../assets/mountain.jpg")}
+            />
         </View>
     )
 }
