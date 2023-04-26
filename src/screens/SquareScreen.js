9c15c3891a2f0ef185e246c02ca57ef2dc778a2c
Sajal Kaushal <sajalkaushal3@gmail.com>
2023-04-26 14:51:21 +0530

    learning reducer

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,52 +1,41 @@
-import React,{useState} from "react";
+import React,{useReducer} from "react";
 import {Text,View,StyleSheet,Button} from 'react-native'
 import ColorCounter from "./components/ColorCounter";
 const COLOR_INCREMENT = 15;
-const  squarescreen = () =>{
-    const [red,setRed] = useState(0)
-    const [blue,setBlue] = useState(0)
-    const [Green,setGreen] = useState(0)
-    const setcolor = (color,change) =>  {
-        // color === red,green,blue
-        // change === +15,-15
-        if(color === 'red'){
-           if(red + change > 255 || red + change <0){
-             return;
-           }else{
-              setRed(red + change);
-           }
-        }
-        if(color === "blue"){
-            if(blue + change > 255 || blue + change <0){
-              return;
-            }else{
-               setBlue(blue+change);
-            }
-         }
-         if(color === "Green"){
-            if(Green + change > 255 || Green + change <0){
-              return;
-            }else{
-               setGreen(Green+change);
-            }
-         }
+// argument 1 -> state === {red:number,green:number,blue:number}
+// argument 2 -> action[how to change state object] === {colortochange:"reg" || "green" || "blue" , amount:15||-15}
+const reducer = (state,action) =>{
+    switch(action.colorToChange){
+        case "red":
+            return {...state , red:state.red + action.amount}
+        case "green" :
+            return {...state , green:state.green + action.amount}
+        case "blue":
+            return {...state , green:state.green + action.amount}
+        default:
+            return state  
     }
-
+}
+const  squarescreen = () =>{
+    // dispatch -> run my  reducer
+const [state,dispatch] =useReducer(reducer,{red:0 , green:0 , blue:0});
+console.log(state) // {red:0.green:0,blue:0}
+const {red,green,blue} = state
     return(
         <View>
            <ColorCounter 
-           onIncrese ={()=> setcolor("red",COLOR_INCREMENT)}
-           onDecrese ={()=> setcolor("red",-1*COLOR_INCREMENT)}
+           onIncrese ={()=> dispatch({colorToChange:"red",amount:COLOR_INCREMENT}) }
+           onDecrese ={()=> dispatch({colorToChange:"red",amount:-1*COLOR_INCREMENT})}
            color="Red"/>
            <ColorCounter
-             onIncrese ={()=> setcolor("blue",COLOR_INCREMENT)}
-             onDecrese ={()=> setcolor("blue", -1*COLOR_INCREMENT)}
+              onIncrese ={()=> dispatch({colorToChange:"blue",amount:COLOR_INCREMENT}) }
+              onDecrese ={()=> dispatch({colorToChange:"blue",amount:-1*COLOR_INCREMENT})}
            color="Blue"/>
            <ColorCounter
-             onIncrese ={()=> setcolor("Green",COLOR_INCREMENT)}
-             onDecrese ={()=> setcolor("Green",-1*COLOR_INCREMENT)}
+              onIncrese ={()=> dispatch({colorToChange:"green",amount:COLOR_INCREMENT}) }
+              onDecrese ={()=> dispatch({colorToChange:"green",amount:-1*COLOR_INCREMENT})}
            color="Green"/>
-        <View style={{height:150,width:150,backgroundColor:`rgb(${red},${blue},${Green})`}}/>
+        <View style={{height:150,width:150,backgroundColor:`rgb(${red},${blue},${green})`}}/>
         </View>
     )
 }
